7c33d4db05ac44d54a4450b3f437ee4325e2d97a
gabrielaheimer <38387611+gabrielaheimer@users.noreply.github.com>
2019-05-14 14:32:07 -0300

    Adiciona comentario

--- P2P/negocinho/Testando.java
+++ P2P/negocinho/Testando.java
@@ -6,6 +6,7 @@ import negocinho.*;
 
 public class Testando {
 	public static void main(String[] args) throws UnknownHostException, SocketException {
+		// argumentos sao: socket do cliente A, IP do cliente B, socket do cliente B
 		Cliente A = new Cliente(4999, "localhost", 5001);
 		
 		Thread rcvA = new Reception(A);
